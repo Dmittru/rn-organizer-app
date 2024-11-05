14db583e163dca70e69e335253cb8828d66a617a
CERF <limbooo689@gmail.com>
2024-11-05 23:22:47 +0300

    LoginPage added

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -3,35 +3,6 @@ import {useEffect, useState} from 'react';
 import { Button } from "../components/Button";
 import {createOrSyncTableUsers, SignInOrUp} from '../entities/user/action'
 
-const styles = StyleSheet.create({
-    body: {
-        flex: 1,
-        alignItems: 'center',
-        backgroundColor: '#abc1d1',
-    },
-    text: {
-        textAlign: 'center',
-        fontSize: 52,
-        color: '#1f3954',
-        marginTop: 200,
-        marginBottom: 20,
-        fontWeight: 'bold',
-        lineHeight:52,
-    },
-    input: {
-        width: 300,
-        borderWidth: 1,
-        color: '#f3f3f3',
-        textAlign: 'center',
-        fontSize: 28,
-        paddingVertical: 5,
-        marginBottom: 10,
-        borderRadius: 20,
-        backgroundColor: '#6a87a5',
-        borderColor: 'rgba(75,113,152,0.8)',
-    }
-})
-
 export const LoginPage = ({ navigation }) => {
     const [login, setLogin] = useState('')
     const [password, setPassword] = useState('')
@@ -48,7 +19,6 @@ export const LoginPage = ({ navigation }) => {
         } else {
             try {
                 SignInOrUp(login, password, navigation)
-                console.log(login, password)
             } catch (error) {
                 console.log(error);
             }
@@ -80,3 +50,32 @@ export const LoginPage = ({ navigation }) => {
         </View>
   )
 }
+
+const styles = StyleSheet.create({
+    body: {
+        flex: 1,
+        alignItems: 'center',
+        backgroundColor: '#abc1d1',
+    },
+    text: {
+        textAlign: 'center',
+        fontSize: 52,
+        color: '#1f3954',
+        marginTop: 200,
+        marginBottom: 20,
+        fontWeight: 'bold',
+        lineHeight:52,
+    },
+    input: {
+        width: 300,
+        borderWidth: 1,
+        color: '#f3f3f3',
+        textAlign: 'center',
+        fontSize: 28,
+        paddingVertical: 5,
+        marginBottom: 10,
+        borderRadius: 20,
+        backgroundColor: '#6a87a5',
+        borderColor: 'rgba(75,113,152,0.8)',
+    }
+})
